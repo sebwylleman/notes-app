74791c49a405ae808bc4ee4d9626ab2ba60cbf20
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-05-04 10:43:44 +0100

    testing user-input add note feature

--- src/views/notesView.test.js
+++ src/views/notesView.test.js
@@ -24,4 +24,17 @@ describe('Notes View', () => {
     expect(notes[0].innerText).toEqual('Go to the gym');
     expect(notes[1].innerText).toEqual('Prepare lunch');
   });
+  it('adds a new note', () => {
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    const input = document.querySelector('#add-note-input');
+    input.value = 'This is a new test note';
+
+    input.click();
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].innerText).toEqual(
+      'This is a new test note'
+    );
+  });
 });
