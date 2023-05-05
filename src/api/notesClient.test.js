ca6c754b3a47302429ed5cbf9ff4d360bfd1e189
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-05-05 10:30:44 +0100

    succesfully calls fetch and loads the data

--- src/api/notesClient.test.js
+++ src/api/notesClient.test.js
@@ -1,44 +1,16 @@
 const NotesClient = require('./notesClient');
 
-// This makes `fetch` available to our test
-// (it is not by default, as normally `fetch` is only
-// available within the browser)
 require('jest-fetch-mock').enableMocks();
 
-describe('notesClient class', () => {
-  it('sends back a 200 OK status msg when an API call is made with the correct url', () => {
-    const client = new NotesClient();
-    client.loadNotes((data) => {
-      expect(data.status).toEqual(200);
-      done();
-    });
-  });
-
-  xit('calls fetch and loads data', (done) => {
-    // 1. Instantiate the class
+describe('NotesClient class', () => {
+  it('calls fetch and loads data', (done) => {
     const client = new NotesClient();
 
-    // 2. We mock the response from `fetch`
-    // The mocked result will depend on what your API
-    // normally returns — you want your mocked response
-    // to "look like" as the real response as closely as
-    // possible (it should have the same fields).
-    fetch.mockResponseOnce(
-      JSON.stringify({
-        name: 'Some value',
-        id: 123,
-      })
-    );
+    fetch.mockResponseOnce(JSON.stringify([{ text: 'Note 1', id: 1 }]));
 
-    // 3. We call the method, giving a callback function.
-    // When the HTTP response is received, the callback will be called.
-    // We then use `expect` to assert the data from the server contain
-    // what it should.
-    client.loadData((returnedDataFromApi) => {
-      expect(returnedDataFromApi.name).toBe('Some value');
-      expect(returnedDataFromApi.id).toBe(123);
-
-      // 4. Tell Jest our test can now end.
+    client.loadNotes((returnedDataFromApi) => {
+      expect(returnedDataFromApi[0].text).toBe('Note 1');
+      expect(returnedDataFromApi[0].id).toBe(1);
       done();
     });
   });
