c34664afca7054ad0e72613bf1c51433ba44ca38
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-05-06 07:06:44 +0100

    connection to the node server via client instance

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+const NotesClient = require('./api/notesClient');
 const NotesModel = require('./models/notesModel');
 const NotesView = require('./views/notesView');
 
 const model = new NotesModel();
 const view = new NotesView(model);
+const client = new NotesClient();
